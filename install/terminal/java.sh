2c49aed576ac63fd07f58921ea20200321becf10
Dave Kirk <me@kirkdave.com>
2024-11-07 11:57:28 +0000

    Handle re-run when configuring Java install

--- install/terminal/java.sh
+++ install/terminal/java.sh
@@ -1,7 +1,7 @@
 mise install java@23
-sudo mkdir /Library/Java/JavaVirtualMachines/23.0.1.jdk
-sudo ln -s /Users/davekirk/.local/share/mise/installs/java/23.0.1/Contents /Library/Java/JavaVirtualMachines/23.0.1.jdk/Contents
+sudo mkdir -p /Library/Java/JavaVirtualMachines/23.0.1.jdk
+sudo ln -sf /Users/davekirk/.local/share/mise/installs/java/23.0.1/Contents /Library/Java/JavaVirtualMachines/23.0.1.jdk/Contents
 
 mise use --global java@21
-sudo mkdir /Library/Java/JavaVirtualMachines/21.0.2.jdk
-sudo ln -s /Users/davekirk/.local/share/mise/installs/java/21.0.2/Contents /Library/Java/JavaVirtualMachines/21.0.2.jdk/Contents
+sudo mkdir -p /Library/Java/JavaVirtualMachines/21.0.2.jdk
+sudo ln -sf /Users/davekirk/.local/share/mise/installs/java/21.0.2/Contents /Library/Java/JavaVirtualMachines/21.0.2.jdk/Contents
