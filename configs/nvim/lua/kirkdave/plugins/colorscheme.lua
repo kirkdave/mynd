95c76cef1798c2f7567e1d1c7480ac2913b4c4a6
Dave Kirk <me@kirkdave.com>
2024-11-14 09:15:46 +0000

    Set Some Settings for Nord Colorscheme

--- configs/nvim/lua/kirkdave/plugins/colorscheme.lua
+++ configs/nvim/lua/kirkdave/plugins/colorscheme.lua
@@ -3,7 +3,13 @@ return {
     {
         "shaunsingh/nord.nvim",
         config = function()
-            vim.cmd([[colorscheme nord]])
+            vim.g.nord_contrast = true
+            vim.g.nord_borders = true
+            vim.g.nord_disable_background = true
+            vim.g.nord_italic = false
+
+            require("nord").set()
+            -- vim.cmd([[colorscheme nord]])
         end,
     },
 }
