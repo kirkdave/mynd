dd0de66bf80e2d8a1134c386f46b864fd3c5d6ab
Dave Kirk <me@kirkdave.com>
2024-11-16 20:40:44 +0000

    Fix Java Installs

--- install/terminal/java.sh
+++ install/terminal/java.sh
@@ -1,9 +1,9 @@
 mise install java@23
 sudo mkdir -p /Library/Java/JavaVirtualMachines/23.0.1.jdk
-sudo ln -sf ~/.local/share/mise/installs/java/23.0.1/Contents /Library/Java/JavaVirtualMachines/23.0.1/Contents
+sudo ln -sf ~/.local/share/mise/installs/java/23.0.1/Contents /Library/Java/JavaVirtualMachines/23.0.1.jdk/Contents
 
 mise install java@21
 sudo mkdir -p /Library/Java/JavaVirtualMachines/21.0.2.jdk
-sudo ln -sf ~/.local/share/mise/installs/java/21.0.2/Contents /Library/Java/JavaVirtualMachines/21.0.2/Contents
+sudo ln -sf ~/.local/share/mise/installs/java/21.0.2/Contents /Library/Java/JavaVirtualMachines/21.0.2.jdk/Contents
 
 mise use --global java@21
