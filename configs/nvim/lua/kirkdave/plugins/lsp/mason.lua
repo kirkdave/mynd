a0fd4ec5bb68fa973d886a9e31818245e8692804
Dave Kirk <me@kirkdave.com>
2024-11-21 17:28:58 +0000

    Fix Loading Order Of Mason Plugin for NeoVim

--- configs/nvim/lua/kirkdave/plugins/lsp/mason.lua
+++ configs/nvim/lua/kirkdave/plugins/lsp/mason.lua
@@ -1,48 +1,56 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		local mason = require("mason")
-		local mason_lspconfig = require("mason-lspconfig")
-		local mason_tool_installer = require("mason-tool-installer")
+	{
+		"williamboman/mason.nvim",
 
-		mason.setup({
-			ui = {
-				icons = {
-					package_installed = "", -- \uf058
-					package_pending = "", -- \uf0a9
-					package_uninstalled = "", -- \uf530
+		config = function()
+			local mason = require("mason")
+
+			mason.setup({
+				ui = {
+					icons = {
+						package_installed = "", -- \uf058
+						package_pending = "", -- \uf0a9
+						package_uninstalled = "", -- \uf530
+					},
 				},
-			},
-		})
+			})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+		},
+		config = function()
+			local mason_lspconfig = require("mason-lspconfig")
+			local mason_tool_installer = require("mason-tool-installer")
 
-		mason_lspconfig.setup({
-			ensure_installed = {
-				"cssls",
-				"cucumber_language_server",
-				"docker_compose_language_service",
-				"dockerls",
-				"html",
-				"java_language_server",
-				"jsonls",
-				"lua_ls",
-				"marksman",
-				"prismals",
-				"ruby_lsp",
-				"ts_ls",
-			},
-		})
+			mason_lspconfig.setup({
+				ensure_installed = {
+					"cssls",
+					"cucumber_language_server",
+					"docker_compose_language_service",
+					"dockerls",
+					"html",
+					"jdtls",
+					"jsonls",
+					"lua_ls",
+					"marksman",
+					"prismals",
+					"ruby_lsp",
+					"ts_ls",
+				},
+			})
 
-		mason_tool_installer.setup({
-			ensure_installed = {
-				"erb-formatter",
-				"prettier",
-				"rubocop",
-				"stylua",
-			},
-		})
-	end,
+			mason_tool_installer.setup({
+				ensure_installed = {
+					"erb-formatter",
+					"prettier",
+					"rubocop",
+					"stylua",
+				},
+			})
+		end,
+	},
 }
