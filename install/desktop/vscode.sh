b0528f558bd97bc729da51b30bd502ce1c0ed28b
Dave Kirk <me@kirkdave.com>
2024-11-21 17:29:41 +0000

    Escape Space if VSCode User Settings Path

--- install/desktop/vscode.sh
+++ install/desktop/vscode.sh
@@ -1,3 +1,3 @@
-mkdir -p ~/Library/Application Support/Code/User
+mkdir -p ~/Library/Application\ Support/Code/User
 
-cp -f ~/.local/share/mynd/configs/vscode/settings.json ~/Library/Application Support/Code/User/settings.json
+cp -f ~/.local/share/mynd/configs/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
