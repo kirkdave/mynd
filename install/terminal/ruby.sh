125439ed081a18ca81c071b97eac550260c1f92d
Dave Kirk <me@kirkdave.com>
2024-11-21 11:54:54 +0000

    Install VSCode Extensions

--- install/terminal/ruby.sh
+++ install/terminal/ruby.sh
@@ -3,3 +3,4 @@ mise use --global ruby@3.3
 
 mise x ruby -- gem install rails --no-document
 mise x ruby -- gem install bundler --no-document
+mise x ruby -- gem install htmlbeautifier --no-document
